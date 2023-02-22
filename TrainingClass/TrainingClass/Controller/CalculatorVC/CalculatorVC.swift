c1bb1e9a82c45f2d8e1b54c027dc30fa168b8fa9
Dmitry Satskevich <Satsckevich1993@yandex.ru>
2023-02-22 22:47:31 +0300

    Working on logic CalculatorVC

--- TrainingClass/TrainingClass/Controller/CalculatorVC/CalculatorVC.swift
+++ TrainingClass/TrainingClass/Controller/CalculatorVC/CalculatorVC.swift
@@ -7,46 +7,84 @@
 
 import UIKit
 
-class CalculatorVC: UIViewController {
+final class CalculatorVC: UIViewController {
     
-    @IBOutlet weak var proteinRateLbl: UILabel!
-    @IBOutlet weak var fatRateLbl: UILabel!
-    @IBOutlet weak var carbohydrateRateLbl: UILabel!
-    @IBOutlet weak var totalCaloriesLbl: UILabel!
+    @IBOutlet weak private var proteinRateLbl: UILabel!
+    @IBOutlet weak private var fatRateLbl: UILabel!
+    @IBOutlet weak private var carbohydrateRateLbl: UILabel!
+    @IBOutlet weak private var totalCaloriesLbl: UILabel!
     
-    @IBOutlet weak var weightLossTFOutlet: UITextField!
-    @IBOutlet weak var weightMaintenanceTFOutlet: UITextField!
-    @IBOutlet weak var massGainTFOutlet: UITextField!
+    @IBOutlet weak private var weightLossTFOutlet: UITextField!
+    @IBOutlet weak private var weightMaintenanceTFOutlet: UITextField!
+    @IBOutlet weak private var massGainTFOutlet: UITextField!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        notificationCenter()
     }
     
     @IBAction func weightLossButton() {
-        weightLossTFOutlet.isHidden = false
+        isHiddenUpdate(textField: weightLossTFOutlet)
     }
     
     @IBAction func weightMaintenanceButton() {
-        weightMaintenanceTFOutlet.isHidden = false
+        isHiddenUpdate(textField: weightMaintenanceTFOutlet)
     }
     
     @IBAction func massGainButton() {
-        massGainTFOutlet.isHidden = false
+        isHiddenUpdate(textField: massGainTFOutlet)
     }
     
     @IBAction func calculateButton() {
+        guard let weightLoss = weightLossTFOutlet.text,
+              let weightMaintenance = weightMaintenanceTFOutlet.text,
+              let massGain = massGainTFOutlet.text
+        else { return }
+        
+        // нужно создать логику отображения КБЖУ
+        if weightLoss != "" {
+            totalCaloriesLbl.text = weightLoss
+        } else if weightMaintenance != "" {
+            totalCaloriesLbl.text = weightMaintenance
+        } else if massGain != "" {
+            totalCaloriesLbl.text = massGain
+        }
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func isHiddenUpdate(textField: UITextField) {
+        if textField.isHidden == true {
+            textField.isHidden = false
+            textField.text = ""
+        } else {
+            textField.isHidden = true
+        }
     }
-    */
-
+    
+    private func notificationCenter() {
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name:
+                                                UIWindow.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name:
+                                                UIWindow.keyboardWillHideNotification, object: nil)
+    }
+    
+    //  Смещение экрана при появлении клавиатуры
+    @objc func kbDidShow(notification: Notification) {
+        self.view.frame.origin.y = 0
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            self.view.frame.origin.y -= (keyboardSize.height / 2)
+        }
+    }
+        
+    //  При закрытии клавиатура, экран возвращается в исходное положение
+    @objc func kbDidHide() {
+        self.view.frame.origin.y = 0
+    }
+    
+    // Скрытие клавиатуры по тапу за пределами Text Field
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true) // Скрывает клавиатуру, вызванную для любого объекта
+    }
 }
